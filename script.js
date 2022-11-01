12adf413c4e9d506597ce594be3259889393dc8e
Pick1100 <109476582+Pick1100@users.noreply.github.com>
2022-10-31 18:33:42 -0600

    Added to do

---
+++ script.js
@@ -0,0 +1,19 @@
+// TODO
+//Model- How you store your data, structure- array, obj, array.object
+    //In let
+
+
+    
+// Timer with decreasing time for wrong answers
+// Randomizer for questions (A-D)
+// Display Question after Next click
+// Display Answers (A-D)
+// Select correct answer media query (on click correct answer A-D)
+// Ending screen 
+// High score list
+//Try again menu
+// On going score
+// Start screen (overlay)
+function time {
+    
+}
