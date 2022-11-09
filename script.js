6b0c0273f2c995b46498f3dab21731e9870e6f3c
Pick1100 <109476582+Pick1100@users.noreply.github.com>
2022-11-08 18:15:17 -0700

    Attempt One, high score not fully working.

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // TODO 
-// start game function is hidden on click, need to make questions appear in the form of buttons. This can be done ith a for loop and .rand, i =  index array number 
-// using headers (h2) assign values to the different questions to appear on the screen. To randomize and loop, for loop for the questions. With JS create buttons and create for each. Use the index.array  for each answer. ENsure you have the correct RealAnswer
-// Append the html for the divs and java script. Next, using an event listener to figure out the answer selected. Next will check answers function to get the text value of the button the user selects and compares to the array. This can be done iwh if conditions. You can then get a prompt for correct or incorrect, subtract time from the timer on click
 // modify the index var to determine if the last question has been reached, and the length.
 // At end of game, another function for an array for initials and value of the seconds left, using local storage- push. On the high scores page, use loops for local storage to order the scores highest to lowest. On submit on initials, push
 // Model- How you store your data, structure- array, obj, array.object
@@ -13,63 +10,113 @@
 // High score list
 // Try again menu
 // On going score
-// Start screen (overlay)
 // answers.addEventListener('click')var answers = document.getElementById("answers")
-var timer = document.getElementById("timer")
-var start = document.getElementById("start")    
+var timerEl = document.getElementById("timer")
+var startEl = document.getElementById("start")
+var questionEl = document.getElementById("question")
+var answersEl = document.getElementById("answers")
+var highScore = document.getElementById("timer")
+
 var timeRemaining = 60
 var timerID;
+var currentQuestionIndex = 0;
+
 var questions = [
     //   Question:Value Repeat line 8-14 ensure you have commas for i=0 .rand # of items in array
     {
-    'question': 'Which can the DOM NOT do?', 
-    'answers': ['Change content of the HTML','Add and Delete HTML Elements','Modify Elements in the HTML', 'Allow a user to access a webpage via a browser extension'],
-    'correctAnswer': 'Allow a user to access a webpage via a browser extension',
+        question: 'Which can the DOM NOT do?',
+        answers: ['Change content of the HTML', 'Add and Delete HTML Elements', 'Modify Elements in the HTML', 'Allow a user to access a webpage via a browser extension'],
+        correctAnswer: 3,
     },
     {
-    'question' :'Most Common was to declare variables that do not have value changes', 
-    'answers': ['let','var','const', 'var =[]'],
-    'correctAnswer': 'const',
-    },    
-    {
-    'question' :'What is the purpose of a loop', 
-    'answers': ['To repeat tasks based on conditions','to iterate using the i =','To allow for greater usage of math and variables', 'To make the developer struggle to understand JavaScript'],
-    'correctAnswer': 'To repeat tasks based on conditions',
+        question: 'Most Common was to declare variables that do not have value changes',
+        answers: ['let', 'var', 'const', 'var = []'],
+        correctAnswer: 2,
     },
     {
-    'question' :'Where is the best place to link your script to .js?', 
-    'answers': ['At the top before the HTML tag','At the bottom of your HTML','Inside your first <div>', 'Under your .css link'],
-    'correctAnswer': 'At the bottom of your HTML',
-    },  
+        question: 'What is the purpose of a loop',
+        answers: ['To repeat tasks based on conditions', 'to iterate using the i =', 'To allow for greater usage of math and variables', 'To make the developer struggle to understand JavaScript'],
+        correctAnswer: 0,
+    },
     {
-    'question': 'Where can you find the answers to most questions?', 
-    'answers': ['In the forums of Facebook','Bing','Through the BootStrap Library', 'The software documentation or Google'],
-    'correctAnswer': 'The software documentation or Google',
-    },  
-    
+        question: 'Where is the best place to link your script to .js?',
+        answers: ['At the top before the HTML tag', 'At the bottom of your HTML', 'Inside your first <div>', 'Under your .css link'],
+        correctAnswer: 0,
+    },
+    {
+        question: 'Where can you find the answers to most questions?',
+        answers: ['In the forums of Facebook', 'Bing', 'Through the BootStrap Library', 'The software documentation or Google'],
+        correctAnswer: 3,
+    },
+
 ];
-timerID = setInterval(time, 1000)
 
 // Timer with decreasing time for wrong answers
-function time(){
+function time() {
     timeRemaining = timeRemaining - 1;
-     timer.textContent = timeRemaining;
-     if (timer.value == 0) {
-        timer.textCOntent = 0;
+    timerEl.textContent = timeRemaining;
+    if (timeRemaining <= 0) {
+        clearInterval(timerID);
+        timerEl.textContent = 0;
         return;
     }
 
 }
-function startGame(){
- start.addEventListener('click', function(){
-alert("Alert")
-start.classList.add('hide')
 
-})
+function displayQuestion() {
+    var q = questions[currentQuestionIndex];
+    questionEl.textContent = q.question;
+    answersEl.innerHTML = '';
+    for (var i = 0; i < q.answers.length; i++) {
+        var choice = q.answers[i];
+        var btnEl = document.createElement('button');
+        btnEl.textContent = choice;
+        btnEl.dataset.index = i;
+        btnEl.addEventListener("click", function () {
+            if (btnEl.dataset.index == q.correctAnswer) {
+
+                console.log("Correct Answer")
+
+            } else {
+                timeRemaining = timeRemaining - 10;
+                alert("Wrong Answer")
+            }
+            currentQuestionIndex++;
+            if (currentQuestionIndex < questions.length) {
+                displayQuestion();
+            } else {
+                clearInterval(timerID);
+                outerBox.classList.add('hide');
+                highScore();
+
+                //TODO: End the game and go to initials/high score and try again screen
+            }
+        });
+        answersEl.appendChild(btnEl);
+    }
+
 }
 
-startGame()
+function highScore() {
+    var nameEl = prompt("What is your name?"); 
+    var highScore = timeRemaining
+    if (localStorage.HighScore > timeRemaining) {
+        localStorage.setItem(highScore);
+        localStorage.setItem(nameEl);
+        confirm.log(localStorage.highScore, nameEl);
+    } else {
+        localStorage.getItem(highScore);
+        localStorage.getItem(nameEl);
+        confirm.log(localStorage.highScore, nameEl);
+    }
+}
 
+    startEl.addEventListener('click', function () {
+        startEl.classList.add('hide');
+        timerID = setInterval(time, 1000);
+        displayQuestion();
+
+    })
 
 
 
